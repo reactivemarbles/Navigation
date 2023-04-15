98d786ef076a84c60723266fe2ba156198a71a4c
Chris Pulman <chris.pulman@yahoo.com>
2023-04-15 16:18:22 +0100

    Update ViewModelRoutedViewHostMixins.cs

--- src/ViewModel.Core/ViewModelRoutedViewHostMixins.cs
+++ src/ViewModel.Core/ViewModelRoutedViewHostMixins.cs
@@ -11,7 +11,6 @@ using ReactiveMarbles.Extensions;
 
 [assembly: InternalsVisibleTo("ReactiveMarbles.ViewModel.Wpf")]
 [assembly: InternalsVisibleTo("ReactiveMarbles.ViewModel.WinForms")]
-[assembly: InternalsVisibleTo("ReactiveMarbles.ViewModel.XamForms")]
 [assembly: InternalsVisibleTo("ReactiveMarbles.ViewModel.MAUI")]
 [assembly: InternalsVisibleTo("ReactiveMarbles.ViewModel.WinUI")]
 [assembly: InternalsVisibleTo("ReactiveMarbles.ViewModel.Avalonia")]
